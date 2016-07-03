1a200f8f756084ef54f46c709fe822a7e34c0bf6
Adam Binford <adamq43@gmail.com>
2016-07-02 20:37:58 -0400

    Removed unused jade code

--- app.js
+++ app.js
@@ -31,10 +31,6 @@ var nebula = require('./nebula')(io);
 var port = process.env.PORT || 8081;
 app.set('port', port);
 
-/* view engine setup, currently not used */
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
