b0d70554810d001f28079eb747d4e38385107df5
Adam Binford <adamq43@gmail.com>
2016-07-22 13:31:45 -0400

    Moved twitter keys to the twitter pipeline instead of inside the data controller

--- pipelines/twitter.py
+++ pipelines/twitter.py
@@ -10,18 +10,22 @@ def main():
     if len(sys.argv) < 2:
         print "Usage: python main.py <port> <pipeline arguments>"
     
+    access_token = "22705576-1RXheyykqon2L6DgUIBtLcqrqeyb5PzIAiTpkN2Eh"
+    access_token_secret = "lBX6JsFWkB1vYM0V3RHQjuWz9gclBc4ZvWACerDTB8O8h"
+    consumer_key = "eNHZNhaOd0QFkCwKqb2si9Of5"
+    consumer_secret = "Ltj6z3BkDanYnjqZwXCcNKiBSu7MoxUrJrHZgPUsnmBSBVoUNu"
+    
     pipeline = nebula.pipeline.Pipeline()
    
     relevance = nebula.model.ActiveSetModel()
     similarity = nebula.model.SimilarityModel()
-    data_controller = nebula.data.TwitterDataController()
-   
-    corpus = nebula.model.CorpusSetModel()
-    #pipeline.append_model(corpus)
+    data_controller = nebula.data.TwitterDataController(access_token=access_token,
+                                                        access_token_secret=access_token_secret,
+                                                        consumer_key=consumer_key,
+                                                        consumer_secret=consumer_secret)
    
     pipeline.append_model(relevance)
     pipeline.append_model(similarity)
-    #pipeline.append_model(composite)
     pipeline.set_data_controller(data_controller)
     
     connector = nebula.connector.ZeroMQConnector(port=int(sys.argv[1]))
